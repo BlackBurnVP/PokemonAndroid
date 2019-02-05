028ce0ac7f12ca2711c766f28789bdda88396193
Vitalii Pshenychniuk <vitalii.pshenychniuk@gmail.com>
2019-02-05 10:14:25 +0100

    Added drawable Added permission request for user location

--- app/src/main/java/com/vitalii/pokemonandroid/MapsActivity.kt
+++ app/src/main/java/com/vitalii/pokemonandroid/MapsActivity.kt
@@ -1,12 +1,17 @@
 package com.vitalii.pokemonandroid
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.widget.Toast
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
@@ -21,6 +26,42 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        checkPermission()
+
+    }
+var ACCESSLOCATION=123
+    fun checkPermission(){
+        if(Build.VERSION.SDK_INT>=23){
+            if(ActivityCompat.checkSelfPermission(this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+
+                requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),ACCESSLOCATION)
+                return
+            }
+        }
+
+        GetUserLocation()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+
+        when(requestCode){
+            ACCESSLOCATION->{
+                if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    GetUserLocation()
+                }else{
+                    Toast.makeText(this,"We can't access your location",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    fun GetUserLocation(){
+        Toast.makeText(this,"User location is on",Toast.LENGTH_SHORT).show()
+        //TODO: Will implement later
     }
 
     /**
@@ -37,7 +78,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.addMarker(MarkerOptions()
+            .position(sydney)
+            .title("Me")
+            .snippet("Here is my location")
+            .icon(BitmapDescriptorFactory.fromResource(R.drawable.mario)))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,14f))
     }
 }
